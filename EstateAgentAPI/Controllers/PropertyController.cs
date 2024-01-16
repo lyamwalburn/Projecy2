5b5003e569f0e63228b0ac3f5f06c1f74746779e
MaxwellBowie <maxgwb@gmail.com>
2024-01-16 14:12:18 +0000

    added property tests and  updated PropertyController

--- EstateAgentAPI/Controllers/PropertyController.cs
+++ EstateAgentAPI/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using EstateAgentAPI.Buisness.DTO;
 using EstateAgentAPI.Buisness.Services;
+using EstateAgentAPI.Persistence.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -44,6 +45,8 @@ namespace EstateAgentAPI.Controllers
         public ActionResult<PropertyDTO> UpdateProperty(PropertyDTO property)
         {
             property = _propertyService.Update(property);
+            if (property == null)
+                return NotFound();
             return property;
         }
 
--- EstateAgentUnitTests/PropertyUnitTests.cs
+++ EstateAgentUnitTests/PropertyUnitTests.cs
@@ -9,12 +9,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
 
 namespace EstateAgentUnitTests
 {
     public class PropertyUnitTests
     {
         private Mapper _mapper;
+        private IPropertyRepository _repo;
+        private PropertyService _service;
+        private EstateAgentContext _context;
+        private PropertyController _controller;
 
         public PropertyUnitTests()
         {
@@ -22,6 +27,14 @@ namespace EstateAgentUnitTests
             MapperConfiguration configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
             _mapper = new Mapper(configuration);
         }
+        private void Setup(IServiceScope scope)
+        {
+            _repo = scope.ServiceProvider.GetService<IPropertyRepository>();
+            _service = new PropertyService(_repo, _mapper);
+            _context = scope.ServiceProvider.GetService<EstateAgentContext>();
+            _controller = new PropertyController(_service);
+        }
+
         private IServiceProvider GetPropertyServiceProivder()
         {
             ServiceCollection services = new ServiceCollection();
@@ -39,6 +52,7 @@ namespace EstateAgentUnitTests
         {
             return new PropertyDTO
             {
+                Id = 1,
                 Address = "testaddress",
                 PostCode = "testpostcode",
                 Type = "apartment",
@@ -55,19 +69,77 @@ namespace EstateAgentUnitTests
         [Fact]
         public void TestAddProperty()
         {
-            //Arrange
             var services = GetPropertyServiceProivder();
             using (var scope = services.CreateScope())
             {
-                var repo = scope.ServiceProvider.GetService<IPropertyRepository>();
-                var service = new PropertyService(repo, _mapper);
-                var context = scope.ServiceProvider.GetService<EstateAgentContext>();
-                var controller = new PropertyController(service);
+                Setup(scope);
                 //Clear database
-                context.Database.EnsureDeleted();
+                _context.Database.EnsureDeleted();
 
                 var propertyDTO = new PropertyDTO
                 {
+                    Address = "test1address",
+                    PostCode = "test1postcode",
+                    Type = "apartment",
+                    NumberOfBedrooms = 1,
+                    NumerOfBathrooms = 2,
+                    Garden = true,
+                    Price = 1000,
+                    Status = "SOLD",
+                    BuyerID = 4,
+                    SellerID = 11
+                };
+
+                _controller.AddProperty(propertyDTO);
+                var property = _context.Properties.Single();
+
+                Assert.Equal(1, property.Id);
+                Assert.Equal("test1address", property.Address);
+            }
+        }
+
+        [Fact]
+        public void TestGetProperty()
+        {
+            var services = GetPropertyServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                Setup(scope);
+                //Clear database
+                _context.Database.EnsureDeleted();
+                //Add property to db
+                _controller.AddProperty(GetMockProperty());
+
+                var propertyFromDb = _controller.Index().FirstOrDefault();
+
+                Assert.Equal(1, propertyFromDb.Id);
+                Assert.Equal("testaddress", propertyFromDb.Address);
+                Assert.Equal("testpostcode", propertyFromDb.PostCode);
+                Assert.Equal("apartment", propertyFromDb.Type);
+                Assert.Equal(1, propertyFromDb.NumberOfBedrooms);
+               // Assert.Equal(2, propertyFromDb.NumerOfBathrooms);
+                Assert.Equal(true, propertyFromDb.Garden);
+                Assert.Equal(1000, propertyFromDb.Price);
+                Assert.Equal("SOLD", propertyFromDb.Status);
+                Assert.Equal(4, propertyFromDb.BuyerID);
+                Assert.Equal(11, propertyFromDb.SellerID);
+            }
+        }
+
+        [Fact]
+        public void TestGetById()
+        {
+            var services = GetPropertyServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                Setup(scope);
+                //Clear database
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
+
+                var secondProperty = new PropertyDTO
+                {
+                    Id = 2,
                     Address = "testaddress",
                     PostCode = "testpostcode",
                     Type = "apartment",
@@ -79,65 +151,157 @@ namespace EstateAgentUnitTests
                     BuyerID = 4,
                     SellerID = 11
                 };
+                _controller.AddProperty(secondProperty);
 
-                //Act
-                controller.AddProperty(propertyDTO);
-                var property = context.Properties.Single();
+                ActionResult<PropertyDTO> propertyFromId = _controller.GetById(2);
 
-                //Assert
-                Assert.Equal(1, property.Id);
-                Assert.Equal("testaddress", property.Address);
+                Assert.Equal(2, propertyFromId.Value.Id);
+                Assert.Equal("testaddress", propertyFromId.Value.Address);
+                Assert.Equal("testpostcode", propertyFromId.Value.PostCode);
+                Assert.Equal("apartment", propertyFromId.Value.Type);
+                Assert.Equal(1, propertyFromId.Value.NumberOfBedrooms);
+                //Assert.Equal(2, propertyFromId.Value.NumerOfBathrooms);
+                Assert.True(propertyFromId.Value.Garden);
+                Assert.Equal(1000, propertyFromId.Value.Price);
+                Assert.Equal("SOLD", propertyFromId.Value.Status);
+                Assert.Equal(4, propertyFromId.Value.BuyerID);
+                Assert.Equal(11, propertyFromId.Value.SellerID);
+            }
+
+        }
+
+        [Fact]
+        public void TestPutProperty()
+        {
+            var services = GetPropertyServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                Setup(scope);
+                //Clear database
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
+
+                var propertyToUpdate = new PropertyDTO
+                {
+                    Id = 1,
+                    Address = "updated testaddress",
+                    PostCode = "updated testpostcode",
+                    Type = "detached",
+                    NumberOfBedrooms = 2,
+                    NumerOfBathrooms = 1,
+                    Garden = false,
+                    Price = 1500,
+                    Status = "SOLD",
+                    BuyerID = 4,
+                    SellerID = 11
+                };
+
+                _controller.UpdateProperty(propertyToUpdate);
+
+                var propertyFromDb = _repo.FindById(1);
+
+                Assert.Equal(1, propertyFromDb.Id);
+                Assert.Equal("updated testaddress", propertyFromDb.Address);
+                Assert.Equal("updated testpostcode", propertyFromDb.PostCode);
+                Assert.Equal("detached", propertyFromDb.Type);
+                Assert.Equal(2, propertyFromDb.NumberOfBedrooms);
+                //Assert.Equal(1, propertyFromDb.NumberOfBathrooms);
+                Assert.False(propertyFromDb.Garden);
+                Assert.Equal(1500, propertyFromDb.Price);
+                Assert.Equal("SOLD", propertyFromDb.Status);
+                Assert.Equal(4, propertyFromDb.BuyerId);
+                Assert.Equal(11, propertyFromDb.SellerId);
+            }
+
+        }
+
+        [Fact]
+        public void TestDeleteProperty()
+        {
+            var services = GetPropertyServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                Setup(scope);
+                //Clear database
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
+
+                var expectedRecordsCount = 0;
+
+                _controller.DeleteProperty(1);
+
+                var propertiesFromDb = _controller.Index();
+
+                Assert.Equal(expectedRecordsCount, propertiesFromDb.Count());
             }
         }
 
         [Fact]
-        public void TestGetProperty()
+        public void Test404ResponseGetPropertyById()
         {
-            //Arrange
             var services = GetPropertyServiceProivder();
             using (var scope = services.CreateScope())
             {
-                var repo = scope.ServiceProvider.GetService<IPropertyRepository>();
-                var service = new PropertyService(repo, _mapper);
-                var context = scope.ServiceProvider.GetService<EstateAgentContext>();
-                var controller = new PropertyController(service);
-                //Clear database
-                context.Database.EnsureDeleted();
+                Setup(scope);
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
 
-                //Act
-                controller.AddProperty(GetMockProperty());
+                var actionResult = _controller.GetById(99);
 
-                //Assert
-                Assert.Equal(1, context.Properties.Count());
-                Assert.Equal("testaddress", context.Properties.FirstOrDefault().Address);
+                var result = actionResult.Result as NotFoundResult;
+                Assert.NotNull(result);
+                Assert.Equal(404, result.StatusCode);
             }
         }
 
         [Fact]
-        public void TestGetById()
+        public void Test404ResponseUpdateProperty()
         {
-            //Arrange
             var services = GetPropertyServiceProivder();
             using (var scope = services.CreateScope())
             {
-                var repo = scope.ServiceProvider.GetService<IPropertyRepository>();
-                var service = new PropertyService(repo, _mapper);
-                var context = scope.ServiceProvider.GetService<EstateAgentContext>();
-                var controller = new PropertyController(service);
-                //Clear database
-                context.Database.EnsureDeleted();
+                Setup(scope);
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
 
-                //Act
-                controller.AddProperty(GetMockProperty());
+                var propertyToUpdate = new PropertyDTO
+                {
+                    Id = 100,
+                    Address = "updated testaddress",
+                    PostCode = "updated testpostcode",
+                    Type = "detached",
+                    NumberOfBedrooms = 2,
+                    NumerOfBathrooms = 1,
+                    Garden = false,
+                    Price = 1500,
+                    Status = "SOLD",
+                    BuyerID = 4,
+                    SellerID = 11
+                };
 
-                ActionResult<PropertyDTO> p = controller.GetById(1);
+                var actionResult = _controller.UpdateProperty(propertyToUpdate);
 
-                //Assert
-                //var result = p.Result as OkObjectResult;
-
-                Assert.Equal("testaddress", p.Value.Address);
+                var result = actionResult.Result as NotFoundResult;
+                Assert.NotNull(result);
+                Assert.Equal(404, result.StatusCode);
             }
+        }
 
+        [Fact]
+        public void Test404ResponseDeleteBuyer()
+        {
+            var services = GetPropertyServiceProivder();
+            using (var scope = services.CreateScope())
+            {
+                Setup(scope);
+                _context.Database.EnsureDeleted();
+                _controller.AddProperty(GetMockProperty());
+
+                var actionResult = _controller.DeleteProperty(100);
+                ;
+                Assert.NotNull(actionResult);
+                Assert.Equal(HttpStatusCode.NotFound, actionResult);
+            }
         }
 
     }
